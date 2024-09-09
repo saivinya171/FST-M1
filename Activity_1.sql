505f7dcc063474743e60af5d555591d2bc6dec0a
saivinya171 <challagundla.sai.vinya@ibm.com>
2024-09-09 11:49:56 +0530

    Add files via upload

---
+++ Activity_1.sql
@@ -0,0 +1,9 @@
+--Activity_1
+CREATE TABLE salesman (
+    salesman_id int,
+    salesman_name varchar2(20),
+    salesman_city varchar2(20),
+    commission int
+    
+);
+DESCRIBE salesman;
---
+++ Activity_3.sql
@@ -0,0 +1,5 @@
+--Activity_3
+
+SELECT salesman_id, salesman_city from salesman;
+SELECT * FROM salesman WHERE salesman_city = 'Paris';
+SELECT salesman_id, commission FROM salesman WHERE salesman_name = 'Paul Adam';
