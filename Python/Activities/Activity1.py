81306261404789a7b060aca6c880ffea08503a5c
saivinya171 <challagundla.sai.vinya@ibm.com>
2024-09-18 21:40:30 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+Name = input ("Enter your Name: ")
+Age = int (input ("Enter your Age: "))
+year = str ((2024-Age)+100)
+print(Name+ " you will be 100 in "+year  )
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the Number: "))
+result = number % 2
+if result>0:
+    print("It is an Odd Number")
+else:
+    print("It is an Even Number")
